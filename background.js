313ca69e3e14072350781a76a3a5b92222de1b98
oneralon <oneralon@gmail.com>
2019-07-11 09:23:27 +0300

    Indents fix

--- background.js
+++ background.js
@@ -2,17 +2,17 @@ var hidden = false;
 
 if (chrome.browserAction) {
   chrome.browserAction.onClicked.addListener(function (tab) {
-	if (/https\:\/\/.*trello\.com\//.test(tab.url)) {
-	  hidden = !hidden;
+  	if (/https\:\/\/.*trello\.com\//.test(tab.url)) {
+  	  hidden = !hidden;
 
-	  var name = hidden ? 'icons/enabled-' : 'icons/disabled-';
-	  chrome.browserAction.setIcon({ path: {
-      "16": name + '16.png',
-      "48": name + '48.png',
-      "128": name + '128.png'
-    } });
+  	  var name = hidden ? 'icons/enabled-' : 'icons/disabled-';
+  	  chrome.browserAction.setIcon({ path: {
+        "16": name + '16.png',
+        "48": name + '48.png',
+        "128": name + '128.png'
+      } });
 
-	  chrome.tabs.sendMessage(tab.id, { action: 'trello-lists-toogle', hide: hidden });
-	}
+  	  chrome.tabs.sendMessage(tab.id, { action: 'trello-lists-toogle', hide: hidden });
+  	}
   });
 }
